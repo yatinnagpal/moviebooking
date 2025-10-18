c7bd801e85532d90f0cb6ed34f6db7044a3130e7
yatinnagpal <yatinnagpal2016@gmail.com>
2025-10-18 11:42:13 +0530

    added showtime service

---
+++ backend/showtime_service/seriallizers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from showtime_service.models import ShowTime
+
+class ShowtimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShowTime
+        fields = '__all__'
