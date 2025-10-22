6d80b0ac0369f6d1574689eba404ee4dd240c656
yatinnagpal <yatinnagpal2016@gmail.com>
2025-10-22 13:12:35 +0000

    add showtime service

--- frontend/movie-booking/src/api.js
+++ frontend/movie-booking/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.API_BASE_URL || "http://localhost:8000/api",
+  baseURL: process.env.API_BASE_URL || "http://localhost:8003/api",
   headers: { "Content-Type": "application/json" },
 });
 
