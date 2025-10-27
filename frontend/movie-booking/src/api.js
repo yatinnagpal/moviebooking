bd0170c2d67ba42495e8062686f8d1c70099a67b
yatinnagpal <yatinnagpal2016@gmail.com>
2025-10-28 00:12:11 +0530

    Prepare frontend for deployment

--- frontend/movie-booking/src/api.js
+++ frontend/movie-booking/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.API_BASE_URL || 'http://localhost:8001/api',
+  baseURL: process.env.API_BASE_URL || 'https://moviebooking-15wj.onrender.com/',
   headers: { 'Content-Type': 'application/json' },
 });
 
